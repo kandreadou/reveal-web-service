7c7d7787872e06a9faed93346475feb22d45db13
kandreadou <kandreadou@iti.gr>
2014-07-25 10:27:20 +0300

    IndexingManager

--- src/main/java/gr/iti/mklab/reveal/HelloController.java
+++ src/main/java/gr/iti/mklab/reveal/HelloController.java
@@ -5,6 +5,7 @@ import eu.socialsensor.framework.client.dao.MediaItemDAO;
 import eu.socialsensor.framework.client.dao.impl.MediaItemDAOImpl;
 import eu.socialsensor.framework.common.domain.MediaItem;
 import gr.iti.mklab.reveal.mongo.MongoManager;
+import gr.iti.mklab.reveal.visual.IndexingManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -18,56 +19,68 @@ public class HelloController {
 
     protected MediaItemDAO mediaDao;
 
-    protected MongoManager mgr = new MongoManager("127.0.0.1", "Linear", "MediaItems");
+    //protected MongoManager mgr = new MongoManager("127.0.0.1", "Linear", "MediaItems");
 
-    public HelloController(){
-        String mongoHost ="127.0.0.1";
+    public HelloController() {
+        String mongoHost = "127.0.0.1";
         try {
             mediaDao = new MediaItemDAOImpl(mongoHost);
-        }catch(Exception ex){
+        } catch (Exception ex) {
             //ignore
         }
     }
 
     @RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello everybody!");
-		return "hello";
-	}
+    public String printWelcome(ModelMap model) {
+        model.addAttribute("message", "Hello everybody!");
+        return "hello";
+    }
 
-    @RequestMapping(value = "/greeting",  method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         return new Greeting(5, "test");
     }
 
-    @RequestMapping(value = "/media",  method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/media", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public List<MediaItem> mediaItems(@RequestParam(value="name", required=false, defaultValue="World") String name) {
+    public List<MediaItem> mediaItems(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         List<MediaItem> list = mediaDao.getLastMediaItems(10);
         return list;
     }
 
-    @RequestMapping(value = "/media/image/{id}",  method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/media/image/{id}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public MediaItem mediaItemById(@PathVariable("id") String mediaItemId) {
         MediaItem mi = mediaDao.getMediaItem(mediaItemId);
         return mi;
     }
 
-    @RequestMapping(value = "/media/image/search",  method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/media/image/search", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public List<MediaItem> mediaItemsSearch(
-            @RequestParam(value="date", required=false) long date) {
+            @RequestParam(value = "date", required = false) long date) {
         List<MediaItem> list = mediaDao.getLastMediaItems(50);
         return list;
     }
 
-    @RequestMapping(value = "/media/test",  method = RequestMethod.GET, produces="application/json")
+    @RequestMapping(value = "/collections/add", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String collectionsAdd(
+            @RequestParam(value = "name", required = true) String name) {
+        try {
+            IndexingManager.createIndex(name);
+            return "success";
+        }catch(Exception ex){
+            return ex.getMessage();
+        }
+    }
+
+    /*@RequestMapping(value = "/media/test", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public List<DBObject> mediaFromManager() {
-       return mgr.search();
-    }
+        return mgr.search();
+    }*/
 
 
     private static final String template = "Hello, %s!";
--- src/main/java/gr/iti/mklab/reveal/visual/IndexingManager.java
+++ src/main/java/gr/iti/mklab/reveal/visual/IndexingManager.java
@@ -4,7 +4,9 @@ import eu.socialsensor.framework.client.dao.impl.MediaItemDAOImpl;
 import eu.socialsensor.framework.client.search.visual.VisualIndexHandler;
 import gr.iti.mklab.visual.aggregation.VladAggregatorMultipleVocabularies;
 import gr.iti.mklab.visual.datastructures.AbstractSearchStructure;
+import gr.iti.mklab.visual.datastructures.IVFPQ;
 import gr.iti.mklab.visual.datastructures.Linear;
+import gr.iti.mklab.visual.datastructures.PQ;
 import gr.iti.mklab.visual.dimreduction.PCA;
 import gr.iti.mklab.visual.extraction.AbstractFeatureExtractor;
 import gr.iti.mklab.visual.extraction.SURFExtractor;
@@ -19,7 +21,7 @@ public class IndexingManager {
     protected static int targetLengthMax = 1024;
     protected static AbstractSearchStructure index;
 
-    protected static void init(boolean train) throws Exception {
+    public static void createIndex(String name) throws Exception {
         String learningFolder = "/home/kandreadou/webservice/learning_files/";
 
 
@@ -35,22 +37,32 @@ public class IndexingManager {
 
         String pcaFile = learningFolder + "pca_surf_4x128_32768to1024.txt";
 
-        //Initialize the ImageVectorization
+
+        int maximumNumVectors = 1000;
+        int m2 = 64;
+        int k_c = 256;
+        int numCoarseCentroids = 8192;
+        String coarseQuantizerFile2 = learningFolder + "qcoarse_1024d_8192k.csv";
+        String productQuantizerFile2 = learningFolder + "pq_1024_64x8_rp_ivf_8192k.csv";
+        String ivfpqIndexFolder = learningFolder + name + "_" + targetLengthMax;
+        IVFPQ ivfpq_1 = new IVFPQ(targetLengthMax, maximumNumVectors, false, ivfpqIndexFolder, m2, k_c, PQ.TransformationType.RandomPermutation, numCoarseCentroids, true, 0);
+        ivfpq_1.loadCoarseQuantizer(coarseQuantizerFile2);
+        ivfpq_1.loadProductQuantizer(productQuantizerFile2);
+        int w = 64; // larger values will improve results/increase seach time
+        ivfpq_1.setW(w); // how many (out of 8192) lists should be visited during search.
+        //visualIndex = new Linear(targetLengthMax, 10000000, false, BDBEnvHome, false, false, 0);
+        //int existingVectors = visualIndex.getLoadCounter();
         SURFExtractor extractor = new SURFExtractor();
         ImageVectorization.setFeatureExtractor(extractor);
         ImageVectorization.setVladAggregator(new VladAggregatorMultipleVocabularies(codebookFiles,
                 numCentroids, AbstractFeatureExtractor.SURFLength));
-
-
         if (targetLengthMax < initialLength) {
             System.out.println("targetLengthMax : " + targetLengthMax + " initialLengh " + initialLength);
             PCA pca = new PCA(targetLengthMax, 1, initialLength, true);
             pca.loadPCAFromFile(pcaFile);
             ImageVectorization.setPcaProjector(pca);
         }
-
-        String BDBEnvHome = learningFolder + "webserv_" + targetLengthMax;
-        index = new Linear(targetLengthMax, 6000, false, BDBEnvHome, true,
-                true, 0);
+        //vectorizer = new ImageVectorizer("surf", "no", codebookFiles,
+        //numCentroids, targetLengthMax, pcaFile, 50);
     }
 }
